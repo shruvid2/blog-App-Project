6784d4decb5d52950905316952e7918f4bf468e1
Shruvi D <56840419+shruvid2@users.noreply.github.com>
2021-01-14 10:49:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () =>
+  console.log("MongoDB connection established seccessfully!")
+);
+
+const articlesRouter = require("./routes/articles");
+app.use("/articles", articlesRouter);
+
+app.listen(port, () => console.log(`The app is running on Port: ${port}`));
